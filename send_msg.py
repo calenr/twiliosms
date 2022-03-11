84f7cfdf290677de5707646b4a4bc3db89b6cd67
Calen Robinson <lokera6@gmail.com>
2022-03-11 15:26:27 -0800

    Add error handling

--- send_msg.py
+++ send_msg.py
@@ -9,9 +9,9 @@ from twilio.rest import Client
 
 def parse_args(*args):
     parser = ap.ArgumentParser()
-    parser.add_argument('--sender',type=str,
+    parser.add_argument('--sender',type=str, required=True,
                         help="The phone number to send SMS from. Must be an active number in your twilio account. Must be in format +12223334444")
-    parser.add_argument('--recvr',type=str,
+    parser.add_argument('--recvr',type=str, required=True,
                         help="The phone number to send SMS to. Must be in format +12223334444")
     parser.add_argument('--body',type=str,default="This message send via python script",
                         help="The body of the text message")
@@ -19,19 +19,23 @@ def parse_args(*args):
     return args
 
 def send_message(*args):
-    args = parse_args(*args)
-    # Find your Account SID and Auth Token at twilio.com/console
-    # and set the environment variables. See http://twil.io/secure
-    account_sid = os.environ['TWILIO_ACCOUNT_SID']
-    api_key = os.environ['TWILIO_API_KEY']
-    api_secret = os.environ['TWILIO_API_KEY_SECRET']
-    client = Client(api_key, api_secret, account_sid)
+    try:
+        args = parse_args(*args)
+        # Find your Account SID and Auth Token at twilio.com/console
+        # and set the environment variables. See http://twil.io/secure
+        account_sid = os.environ['TWILIO_ACCOUNT_SID']
+        api_key = os.environ['TWILIO_API_KEY']
+        api_secret = os.environ['TWILIO_API_KEY_SECRET']
+        client = Client(api_key, api_secret, account_sid)
 
-    message = client.messages.create(
-                                from_=args.sender,
-                                body=args.body,
-                                to=args.recvr
-                            )
+        message = client.messages.create(
+                                    from_=args.sender,
+                                    body=args.body,
+                                    to=args.recvr
+                                    )
+    except Exception as e:
+        print("Error: Could not send message via twilio")
+        print(e)
 
 if __name__ == "__main__":
     send_message(sys.argv[1:])
